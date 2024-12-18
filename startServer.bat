0511273e9f7ca90155c055adceedd9a33cfec19e
tusarimrananik <tusarimrananik@gmail.com>
2024-12-18 20:40:30 +0600

    changes in packages.json

--- startServer.bat
+++ startServer.bat
@@ -1,3 +1,3 @@
 @echo off
-start "FBClone Server" /min cmd /C "cd /d %USERPROFILE%\Desktop\FBClone && npm start"
+start "SocialClone Server" /min cmd /C "cd /d %USERPROFILE%\Desktop\SocialClone && npm start"
 start "Ngrok Server" /min cmd /C "ngrok http --url=humane-newt-formally.ngrok-free.app 3000"
